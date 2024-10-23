10872772801d9892c465ea84f098129510c586ae
iHuzaifaEhsan <iamhuzaifaehsan@gmail.com>
2024-10-23 16:14:52 +0500

    latest commit

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
+import { asyncloadmovie } from '../store/actions/movieActions'
+import { useParams } from 'react-router-dom'
 
 const Moviedetails = () => {
-  
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(asyncloadmovie)
+  }, [])
+
   return (
     <div>Moviedetail</div>
   )
