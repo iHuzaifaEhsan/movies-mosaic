aa1f02c9d31ac0c06ba787ffc98ab4dfe2c3bb51
iHuzaifaEhsan <iamhuzaifaehsan@gmail.com>
2024-10-23 16:50:31 +0500

    latest commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Popular from './components/Popular'
 import Movie from './components/Movie'
 import Tvshows from './components/Tvshows'
 import People from './components/People'
-import MovieDetails from './components/Moviedetails'
+import MovieDetails from './components/MovieDetails'
 import TvDetails from './components/TvDetails'
 import PersonDetails from './components/PersonDetails'
 
