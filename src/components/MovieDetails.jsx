3538eae1403f1996549a56fea0e509d21cac1d07
iHuzaifaEhsan <iamhuzaifaehsan@gmail.com>
2024-10-27 12:10:11 +0500

    latest commit

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,12 +1,13 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { asyncloadmovie, removemovie } from '../store/actions/movieActions';
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
 import Loading from './Loader'
 
 const Moviedetails = () => {
   document.title = "Movies Mosaic | Movies";
   const navigate = useNavigate();
+  const { pathname } = useLocation();
   const { info } = useSelector(state => state.movie);
   const { id } = useParams();
   const dispatch = useDispatch();
@@ -37,11 +38,26 @@ const Moviedetails = () => {
           alt=""
         />
 
-        <div className='content ml-[5%]'>
+        <div className='content ml-[5%] text-white'>
           <h1
             className='text-5xl font-black text-white'> {info.detail.name || info.detail.original_name || info.detail.original_title}
-            <small className='text-xl font-bold text-zinc-300'>({info.detail.release_date.split("-")[0]})</small>
+            <small className='text-xl font-bold text-white'>({info.detail.release_date.split("-")[0]})</small>
           </h1>
+          <div className='flex text-zinc-100 items-center gap-x-5 mt-3 mb-5'>
+            <span className='rounded-full text-sm font-semibold text-white w-[5vh] h-[5vh] flex justify-center items-center bg-yellow-600'>
+              {(info.detail.vote_average * 10).toFixed()}<sup>%</sup>
+            </span>
+            <h1 className='w-[60px] font-semibold text-2xl leading-6'>User Score</h1>
+            <h1>{info.detail.release_date}</h1>
+            <h1>{info.detail.genres.map((g) => g.name).join(",")}</h1>
+            <h1>{info.detail.runtime}min</h1>
+          </div>
+          <h1 className='text-xl font-semibold italic text-zinc-200'>{info.detail.tagline}</h1>
+          <h1 className='text-2xl font-semibold mt-5 mb-3'>Overview</h1>
+          <p>{info.detail.overview}</p>
+          <h1 className='text-2xl font-semibold mt-5 mb-3'>Movie Translated</h1>
+          <p className='mb-10'>{info.translations.join(", ")}</p>
+          <Link className='p-5 bg-[#6556CD] rounded-lg' to={`${pathname}/trailer`}><i className=" text-2xl mr-3 ri-play-fill"></i>Play Trailer</Link>
         </div>
 
 
@@ -51,7 +67,7 @@ const Moviedetails = () => {
       <div className='w-[80%] flex flex-col gap-y-5 mt-5'>
         {info.watchproviders && info.watchproviders.flatrate &&
           <div className='flex gap-x-5 items-center text-white'>
-            <h1>Available on Plateforms</h1>
+            <h1>On Plateforms</h1>
             {info.watchproviders.flatrate.map((w, i) => (
               <img title={w.provider_name} className='w-[5vh] h-[5vh] object-cover rounde-md cursor-pointer' key={i} src={`https://image.tmdb.org/t/p/original/${w.logo_path}`} alt="" />
             ))
@@ -60,7 +76,7 @@ const Moviedetails = () => {
 
         {info.watchproviders && info.watchproviders.rent &&
           <div className='flex gap-x-5 items-center text-white'>
-            <h1>Available on Rent</h1>
+            <h1>On Rent</h1>
             {info.watchproviders.rent.map((w, i) => (
               <img title={w.provider_name} className='w-[5vh] h-[5vh] object-cover rounde-md cursor-pointer' key={i} src={`https://image.tmdb.org/t/p/original/${w.logo_path}`} alt="" />
             ))
@@ -69,7 +85,7 @@ const Moviedetails = () => {
 
         {info.watchproviders && info.watchproviders.buy &&
           <div className='flex gap-x-5 items-center text-white'>
-            <h1>Available to Buy</h1>
+            <h1>To Buy</h1>
             {info.watchproviders.buy.map((w, i) => (
               <img title={w.provider_name} className='w-[5vh] h-[5vh] object-cover rounde-md cursor-pointer' key={i} src={`https://image.tmdb.org/t/p/original/${w.logo_path}`} alt="" />
             ))
